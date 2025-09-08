7269f9ae690c2dbc09196c6116d3724ba1df3c42
quietjun802 <whdydwns802@gmail.com>
2025-09-08 21:43:38 +0900

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { logoData, companyData, customerCenterData, footerMenus } from "../util/footer";
+import Nav from './Nav';
+import "../styles/components/footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="inner foot-inner">
+        <div className="left">
+          <h3>
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </h3>
 
-export default Footer
+          <ul className="foot-lst-1">
+            <li></li>
+
+          </ul>
+
+        </div>
+
+        <div className="center">
+          <div className="foot-menus">
+          
+              <div>
+                <h4></h4>
+                <ul>
+                    <li >
+                    </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+        {/* 오른쪽 영역(비워둠 / SNS 등 넣을 자리) */}
+        <div className="right">
+
+          {/* 고객센터 */}
+          <div>
+            <h4></h4>
+            <p className='cs-box'>
+              <a href="#">
+              </a>
+            </p>
+            <p>hours</p>
+            <p>notice</p>
+            <a className='talk-btn' href="#">
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
